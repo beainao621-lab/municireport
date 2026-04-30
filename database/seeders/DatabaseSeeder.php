94cfef112066d7c5b7b33a0e54ab65c0ef293c21
Betty Mae <ikaw@email.com>
2026-04-30 15:30:19 +0800

    feat: seed admin user in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,23 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(AdminSeeder::class);
+         User::updateOrCreate(
+            ['email' => 'admin@municireport.com'],
+            [
+                'name'     => 'Administrator',
+                'email'    => 'admin@municireport.com',
+                'password' => Hash::make('Admin@1234'),
+                'role'     => 'admin',
+                'phone'    => null,
+                'location' => null,
+            ]
+        );
     }
 }
